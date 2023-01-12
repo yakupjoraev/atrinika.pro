bff77ef7da92a336ef62d8a67a5ae06f32df2f00
Yakup <yakupjoraev@gmail.com>
2023-01-12 12:15:50 +0300

    new function

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,3 +1,23 @@
 // Custom Scripts
 // Custom scripts
 
+// функция для constructor-1--1
+function sports() {
+  const container = document.querySelector('.constructor__sports')
+
+  if (!container) {
+    return null
+  }
+
+  let sportLables = document.querySelectorAll('[data-label]')
+
+  sportLables.forEach(sportLable => {
+    addEventListener('click', () => {
+      container.setAttribute("data-checked", "checked")
+    })
+  });
+
+}
+sports();
+
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
 // Custom scripts
+
+// функция для constructor-1--1
+function sports() {
+  const container = document.querySelector('.constructor__sports')
+
+  if (!container) {
+    return null
+  }
+
+  let sportLables = document.querySelectorAll('[data-label]')
+
+  sportLables.forEach(sportLable => {
+    addEventListener('click', () => {
+      container.setAttribute("data-checked", "checked")
+    })
+  });
+
+}
+sports();
+
