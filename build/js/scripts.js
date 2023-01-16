701c7124df5ec87d6bc7f7e7fd74faa88ff549ff
Yakup <yakupjoraev@gmail.com>
2023-01-16 10:38:20 +0300

    slider

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,7 +1,8 @@
 // Custom Scripts
 // Custom scripts
 
-// функция для constructor-1--1
+// функция для constructor--1-1
+
 function sports() {
   const container = document.querySelector('.constructor__sports')
 
@@ -20,4 +21,30 @@ function sports() {
 }
 sports();
 
+// функция для constructor--2-1
 
+let swiper = new Swiper('.constructor__slider-swiper', {
+  slidesPerView: 1,
+  centeredSlides: true,
+  loop: true,
+  pagination: {
+    el: '.constructor__slider-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+  navigation: {
+    nextEl: '.constructor__slider-next',
+    prevEl: '.constructor__slider-prev',
+  },
+
+  breakpoints: {
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    }
+  }
+
+
+});
