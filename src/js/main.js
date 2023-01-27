c924c7344ef405c635eb862bca1d3fc84dc41af2
Yakup <yakupjoraev@gmail.com>
2023-01-27 11:59:14 +0300

    fix select

--- src/js/main.js
+++ src/js/main.js
@@ -109,173 +109,114 @@ $(function () {
   $("#size-sleeve2").selectmenu();
 });
 
-const getTemplate = (data = [], placeholder, selectedId) => {
-  let text = placeholder ?? 'placeholder не указан'
+// СЕЛЕКТЫ
 
-  const items = data.map(item => {
-    let cls = ''
-    if (item.id === selectedId) {
-      text = item.value
-      cls = 'selected'
+class CustomSelector {
+  data = []
+  input = ''
+  select = ''
+  dropdown = ''
+
+  constructor(id, data) {
+    this.data = data
+    this.input = document.querySelector(id)
+  }
+
+  async init() {
+    this.input.value = this.data[0].value
+    this.dropdown = new DOMParser()
+      .parseFromString(await this.templateDropdown(), 'text/html')
+      .querySelector('body')
+      .querySelector('div')
+    this.select = new DOMParser()
+      .parseFromString(await this.templateSelect(), 'text/html')
+      .querySelector('body')
+      .querySelector('div')
+    this.input.after(this.dropdown)
+    this.input.after(this.select)
+    this.event()
+  }
+
+  event() {
+    this.select.addEventListener('click', () => {
+      if (this.dropdown.classList.contains('active')) {
+        this.dropdown.classList.remove('active')
+        this.select.classList.remove('active')
+      } else {
+        this.dropdown.classList.add('active')
+        this.select.classList.add('active')
+      }
+    })
+    this.dropdown.querySelectorAll('[data-option]').forEach((option) => {
+      option.addEventListener('click', (event) => {
+        this.select.querySelector('[data-color]').style.backgroundColor = event.currentTarget.dataset.color
+        this.select.querySelector('span').innerHTML = event.currentTarget.dataset.name
+        this.input.value = event.currentTarget.dataset.value
+        this.dropdown.classList.remove('active')
+        this.select.classList.remove('active')
+      })
+    })
+  }
+
+  setValue() {
+    this.el.value = this.value
+  }
+
+  async templateSelect(data = {}) {
+    let template = `
+              <div data-select class="select__input">
+                <div data-color class="select__color" style="background-color: ${data.color ? data.color : this.data[0].color}"></div>
+                <span data-name>${data.name ? data.name : this.data[0].name}</span>
+              </div>
+            `;
+    return await template
+  }
+
+  async templateDropdown() {
+    let template = `
+              <div data-dropdown class="select__dropdown">
+              <ul class="select__list">
+          `;
+    if (this.data) {
+      this.data.forEach((item, key) => {
+        template += `
+                      <li data-option class="select__item" data-name="${item.name}" data-value="${item.value}" data-color="${item.color}" data-selected="${key == 0 ? true : false}">
+                          <div class="item__color" style="background-color: ${item.color}"></div>
+                          <span>${item.name}</span>
+                      </li>
+                  `;
+      });
     }
-    return `
-          <li class="select__item ${cls}" data-type="item" data-id="${item.id}">
-          <div class="item__color" data-type="color"></div>${item.value}</li>
-      `
-  })
-  return `
-      <input type="hidden" class="hidden__input">
-      <div class="select__backdrop" data-type="backdrop"></div>
-      <div class="select__input" data-type="input">
-          <div class="select__color" data-type="color"></div>
-          <span data-type="value">${text}</span>
-      </div>
-      <div class="select__dropdown">
-          <ul class="select__list">
-              ${items.join('')}
-          </ul>
-      </div>
-  `
-}
-class Select {
-  constructor(selector, options) {
-    this.$el = document.querySelector(selector)
-    this.options = options
-    this.selectedId = options.selectedId
-
-    this.render()
-    this.setup()
-  }
-
-  render() {
-    const { placeholder, data } = this.options
-    this.$el.classList.add('select')
-    this.$el.innerHTML = getTemplate(data, placeholder, this.selectedId)
-  }
-  setup() {
-    this.clickHandler = this.clickHandler.bind(this)
-    this.$el.addEventListener('click', this.clickHandler)
-    this.$value = this.$el.querySelector('[data-type="value"]')
-  }
-
-  clickHandler(event) {
-    const { type } = event.target.dataset
-    if (type === 'input') {
-      this.toggle()
-    } else if (type === 'item') {
-      const id = event.target.dataset.id
-      this.select(id)
-    } else if (type === 'backdrop') {
-      this.close()
-    }
-  }
-
-  get isOpen() {
-    return this.$el.classList.contains('open')
-  }
-
-  get current() {
-    return this.options.data.find(item => item.id === this.selectedId)
-  }
-
-  select(id) {
-    this.selectedId = id
-    this.$value.textContent = this.current.value
-
-    this.$el.querySelectorAll(`[data-type="item"]`).forEach(el => el.classList.remove('selected'))
-    this.$el.querySelector(`[data-id="${id}"]`).classList.add('selected')
-
-    this.options.onSelect ? this.options.onSelect(this.current) : null
-    this.close()
-  }
-
-  toggle() {
-    this.isOpen ? this.close() : this.open()
-  }
-
-  open() {
-    this.$el.classList.add('open')
-  }
-
-  close() {
-    this.$el.classList.remove('open')
-  }
-
-  destroy() {
-    this.$el.removeEventListener('click', this.clickHandler)
-    this.$el.innerHTML = ''
+    template += `
+              </ul>
+              </div>
+          `;
+    return await template
   }
 }
 
-
-// Инициализация плагина
-const select1 = new Select('#color-tshirt', {
-  placeholder: 'Красный',
-  selectedId: '1',
-  data: [
-    { id: '1', value: 'Красный 1' },
-    { id: '2', value: 'Красный 2' },
-    { id: '3', value: 'Красный 3' },
-    { id: '4', value: 'Красный 4' },
-    { id: '5', value: 'Красный 5' }, ,
-  ],
-  onSelect(item) {
-    const input = document.querySelector('.hidden__input')
-    input.value = item.value
+let data = [
+  {
+    name: 'test1',
+    value: 'value1',
+    color: '#FF0000',
+  },
+  {
+    name: 'test2',
+    value: 'value2',
+    color: '#C71585',
+  },
+  {
+    name: 'test3',
+    value: 'value3',
+    color: '#808000',
   }
-
-})
-
-// Инициализация плагина
-const select2 = new Select('#color-num-tshirt', {
-  placeholder: 'Черный',
-  selectedId: '1',
-  data: [
-    { id: '1', value: 'Черный 1' },
-    { id: '2', value: 'Черный 2' },
-    { id: '3', value: 'Черный 3' },
-    { id: '4', value: 'Черный 4' },
-    { id: '5', value: 'Черный 5' }, ,
-  ],
-  onSelect(item) {
-    const input = document.querySelector('.hidden__input')
-    input.value = item.value
-  }
-
-})
-
-// Инициализация плагина
-const select3 = new Select('#color-shorts', {
-  placeholder: 'Белый',
-  selectedId: '1',
-  data: [
-    { id: '1', value: 'Белый 1' },
-    { id: '2', value: 'Белый 2' },
-    { id: '3', value: 'Белый 3' },
-    { id: '4', value: 'Белый 4' },
-    { id: '5', value: 'Белый 5' }, ,
-  ],
-  onSelect(item) {
-    const input = document.querySelector('.hidden__input')
-    input.value = item.value
-  }
-
-})
-
-// Инициализация плагина
-const select4 = new Select('#color-num-shorts', {
-  placeholder: 'Красный',
-  selectedId: '1',
-  data: [
-    { id: '1', value: 'Красный 1' },
-    { id: '2', value: 'Красный 2' },
-    { id: '3', value: 'Красный 3' },
-    { id: '4', value: 'Красный 4' },
-    { id: '5', value: 'Красный 5' }, ,
-  ],
-  onSelect(item) {
-    const input = document.querySelector('.hidden__input')
-    input.value = item.value
-  }
-
-})
+]
+let colorTshirt = new CustomSelector('#color-tshirt', data)
+let colorNumTshirt = new CustomSelector('#color-num-tshirt', data)
+let colorShorts = new CustomSelector('#color-shorts', data)
+let colorNumShorts = new CustomSelector('#color-num-shorts', data)
+colorTshirt.init()
+colorNumTshirt.init()
+colorShorts.init()
+colorNumShorts.init()
