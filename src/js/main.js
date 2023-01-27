4a6c359680fe0fbd7788b169d485bce0565276d2
Yakup <yakupjoraev@gmail.com>
2023-01-27 13:02:52 +0300

    rededighn 1.1

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,41 @@
 // Custom scripts
 
-// функция для constructor--1-1
+// функция для constructor--2-1
 
-function sports() {
-  const container = document.querySelector('.constructor__sports')
+function sliderConstructor() {
+
+  const container = document.querySelector('.constructor__slider')
 
   if (!container) {
     return null
   }
 
-  let sportLables = document.querySelectorAll('[data-label]')
-
-  sportLables.forEach(sportLable => {
-    addEventListener('click', () => {
-      container.setAttribute("data-checked", "checked")
-    })
-  });
-
-}
-sports();
-
-// функция для constructor--2-1
-
-let swiper = new Swiper('.constructor__slider-swiper', {
-  slidesPerView: 1,
-  centeredSlides: true,
-  pagination: {
-    el: '.constructor__slider-pagination',
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + '</span>';
+  let swiper = new Swiper('.constructor__slider-swiper', {
+    slidesPerView: 1,
+    centeredSlides: true,
+    pagination: {
+      el: '.constructor__slider-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
     },
-  },
-  navigation: {
-    nextEl: '.constructor__slider-next',
-    prevEl: '.constructor__slider-prev',
-  },
+    navigation: {
+      nextEl: '.constructor__slider-next',
+      prevEl: '.constructor__slider-prev',
+    },
 
-  breakpoints: {
-    991: {
-      slidesPerView: 3,
-      spaceBetween: 20,
+    breakpoints: {
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      }
     }
-  }
 
-});
+  });
+}
+
+sliderConstructor()
 
 // custom input file
 
